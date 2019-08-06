97f55608d618deca192d071abd9b01a81f9c7cfa
DBIZ\nguyenn <nt.nguyen@lombardodier.com>
2019-08-06 14:00:04 +0200

    Fixes config

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-/*
- * Copyright (c) 2019 by Bank Lombard Odier & Co Ltd, Geneva, Switzerland. This software is subject
- * to copyright protection under the laws of Switzerland and other countries. ALL RIGHTS RESERVED.
- *
- */
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
 const db = require('./api/startup/startups.dao.pg')
-const port = 3000
+const bodyParser = require('body-parser')
+
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use(bodyParser.json())
 app.use(
@@ -17,16 +20,10 @@ app.use(
     })
 )
 
-app.get('/', (request, response) => {
-    response.json({info: 'Node.js, Express, and Postgres API'})
-})
-
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+module.exports = app;
